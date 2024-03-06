0f0d3a5c06f7c6176607a7be93206fc25a35fb83
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-03-05 21:30:57 -0300

    :recycle: refactor: turn footer into component

---
+++ views/components/footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-primary p-3 text-center">
+    <p class="fs-4 text-white m-0">
+      Desenvolvido por <strong>@fe-tabs</strong>
+    </p>
+</footer>
--- views/modules/books/list.php
+++ views/modules/books/list.php
@@ -89,9 +89,5 @@
   </section>
 </main>
 
-<footer class="bg-primary p-3 text-center">
-    <p class="fs-4 text-white m-0">
-      Desenvolvido por <strong>@fe-tabs</strong>
-    </p>
-</footer>
+<?php include_once 'views/components/footer.php';?>
   
